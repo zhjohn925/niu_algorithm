e4646c8aa5c0de653665d1408a069df1893e3a8f
zhjohn925 <zhjohn925@gmail.com>
2023-01-19 17:57:40 -0800

    Update L02_BubbleSort.java

--- L02_BubbleSort.java
+++ L02_BubbleSort.java
@@ -38,4 +38,80 @@ public class Code_00_BubbleSort {
     arr[i] = arr[j];
     arr[j] = tmp;
   }
+  
+  // ------------------------------------------------
+  // for test
+  // ------------------------------------------------
+  
+  // generate array with random size, and random values
+  public static int[] generateRandomArray(int size, int value) {
+    // Math.random() -> double [0,1)
+    // (int)((size+1)*Math.random())  --> [0, size]
+    int[] arr = new int[(int)((size+1)*Math.random())];
+    for (int i=0; i<arr.length; i++) {
+      arr[i] = (int)((value+1) * Math.random()) - (int) (value*Math.random());
+    }
+    return arr;
+  }
+  
+  // call library sort() to get sorted array for reference
+  public static void rightMethod(int[] arr) {
+    Arrays.sort(arr);
+  }
+  
+  public static int[] copyArray(int[] arr) {
+    if (arr == null) return null;
+    int[] res = new int[arr.length];
+    for (int i=0; i<arr.length; i++) {
+      res[i] = arr[i];
+    }
+    return res;
+  }
+  
+  public static boolean isEqual(int[] arr1, int[] arr2) {
+    if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+      return false;
+    }
+    if (arr1 == null && arr2 == null) {
+      return true;
+    }
+    if (arr1.length != arr2.length) {
+      return false;
+    }
+    for (int i=0; i<arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  public static void printArray(int[] arr) {
+    if (arr == null) return;
+    for (int i=0; i<arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+  
+  
+  public static void main(String[] args) {
+    int testTime = 500000;
+    int size = 10;
+    int value = 100;
+    boolean succeed = true;
+    for (int i=0; i<testTime; i++) {
+      int[] arr1 = generateRandomArray(size, value);
+      int[] arr2 = copyArray(arr1);
+      int[] arr3 = copyArray(arr1);
+      bubbleSort(arr1);
+      rightMathod(arr2);
+      if (!isEqual(arr1, arr2)) {
+        succeed = false;
+        printArray(arr3);
+        break;
+      }
+    }
+    System.out.println(succeed ? "Nice!" : "Bad!");
+  
 }
