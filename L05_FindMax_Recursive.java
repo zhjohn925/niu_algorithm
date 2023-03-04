981a266414efe9d8f3727dcf46de0c5ddbf5fc25
zhjohn925 <zhjohn925@gmail.com>
2023-03-03 22:11:36 -0800

    Update and rename L05_temp to L05_FindMax_Recursive.java

---
+++ L05_FindMax_Recursive.java
@@ -0,0 +1,16 @@
+// find max number in an array
+
+public class FindMax {
+   public static int getMax(int[] arr, int L, int R) {
+      if (L==R) return arr[L];
+      int mid = (L+R)/2;
+      int maxLeft = getMax(arr, L, mid);
+      int maxRight = getMax(arr, mid+1, R);
+      return Math.max(maxLeft, maxRight);
+   }
+   
+   public static void main(String[] args) {
+      int[] arr = {4, 3, 2, 1};
+      System.out.println(getMax(arr, 0, arr.length-1));
+   }
+}
