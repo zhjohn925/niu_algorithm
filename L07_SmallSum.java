53692b07389f91bd900901ae63766dc8bedac788
zhjohn925 <zhjohn925@gmail.com>
2023-03-04 10:28:59 -0800

    Update L07_SmallSum.java

--- L07_SmallSum.java
+++ L07_SmallSum.java
@@ -12,6 +12,45 @@
 
 // 如果直接用两层for循环扫一遍，时间复杂度O(N*N)，这个题目可以利用归并排序(Merge Sort)把时间复杂度降到O(N*logN)
 
+public class SmallSum {
+  
+   public static int smallSum(int[] arr) {
+      if (arr == null || arr.length < 2) {
+          return 0;
+      }
+      return mergeSort(arr, 0, arr.length-1);
+   }
+     
+   public static void mergeSort(int[] arr, int L, int R) {
+      if (L == R) {
+          return 0;
+      }
+      int mid = L + ((R-L) >> 1);
+      return mergeSort(arr, L, mid)   +         // T(N/2)
+             mergeSort(arr, mid+1, R) +         // T(N/2)
+             merge(arr, L, mid, R);             // O(N) & Extra Space complexity O(N) using help[]
+   }
+    
+   public static void merge(int[] arr, int L, int mid, int R) {
+      int [] help = new int[R-L+1];
+      int i=0; 
+      int p1=L;
+      int p2=mid+1;
+      while (p1<=mid && p2<=R) {
+         help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+      }
+      while (p1<=mid) {
+         help[i++] = arr[p1++];
+      }
+      while (p2<=R) {
+         help[i++] = arr[p2++];
+      }     
+      for (i=0; i<help.length; i++) {
+         arr[L+i] = help[i];
+      }
+   }  
+}
+
 
 
 
