0b5da92bcfc234a03568d6733a4475ded434b482
zhjohn925 <zhjohn925@gmail.com>
2023-03-09 23:18:43 -0800

    Update L09_QuickSort.java

--- L09_QuickSort.java
+++ L09_QuickSort.java
@@ -16,6 +16,8 @@ public class QuickSort {
   
   public static void quickSort(int[] arr, int L, int R) {
     if (L < R) {
+      // pick an random item (X), put it in the right-most. 
+      // X is used to be middle number
       swap(arr, L + (int)(Math.random() * (R-L+1)), R);
       int[] p = partition(arr, L, R);
       quickSort(arr, L, p[0] - 1);
@@ -23,7 +25,28 @@ public class QuickSort {
     }
   }
   
+  public static int[] partition(int [] arr, int L, int R) {
+    int less = L - 1;
+    int more = R + 1;
+    int cur = L;
+    int num = arr[R];    // the right-most item as middle number
+    while (cur < more) {
+      if (arr[cur] < num) {
+        swap(arr, ++less, cur++);
+      } else if (arr[cur] > num) {
+        swap(arr, --more, cur);
+      } else {
+        cur++;
+      }
+    }
+    return new int[] { less + 1, more - 1};
+  }
   
+  public static void swap(int[] arr, int i, int j) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  } 
   
 }
 
