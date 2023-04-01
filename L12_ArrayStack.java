88725ec9b7bb989b0909ca2452d19bdbcdbba688
zhjohn925 <zhjohn925@gmail.com>
2023-04-01 10:56:23 -0700

    Create L12_ArrayStack.java

---
+++ L12_ArrayStack.java
@@ -0,0 +1,32 @@
+public static class ArrayStack {
+  private Integer[] arr;
+  private Integer index;
+  
+  public ArrayStack(int initSize) {
+    if (initSize < 0) {
+      throw new IllegalArgumentException("The init size is less than 0");
+    }
+    arr = new Integer[initSize];
+    size = 0;
+  }
+  
+  public Integer peek() {
+    if (index==0) return null;
+    reture arr[index-1];
+  }
+  
+  public void push(int obj) {
+    if (size == arr.length) {
+      throw new ArrayIndexOutOfBoundsException("The queue is full");
+    }
+    arr[index++] = obj;
+  }
+  
+  public Integer pop() {
+    if (index == 0) {
+      throw new ArrayIndexOutOfBoundsException("The queue is empty");
+    }
+    return arr[--index];
+  }  
+}
+
