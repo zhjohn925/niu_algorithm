2b15fc0aa066b39782107f010e78aa967f7e1eff
zhjohn925 <zhjohn925@gmail.com>
2023-04-01 11:17:28 -0700

    Create L13_ArrayQueue.java

---
+++ L13_ArrayQueue.java
@@ -0,0 +1,46 @@
+// Use array to implement Queue data structure, FIFO, first in first out
+
+public static class ArrayQueue {
+  private Integer[] arr;
+  private Integer size;    // number of data in the Queue
+  private Integer start;   // index to pop.
+  private Integer end;     // index to push.  consider "start" to catch up "end"
+  
+  public ArrayQueue(int initSize) {
+    if (initSize < 0) {
+      throw new IllegalArgumentException("The init size is less than 0");
+    }
+    arr = new Integer[initSize];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+  public Integer peek() {
+    if (size == 0) {
+      return null;
+    }
+    return arr[start];
+  }
+  
+  public void push(int obj) {
+    if (size == arr.length) {
+      throw new ArrayIndexOutOfBoundsException("The queue is full");
+    }
+    size++;
+    arr[end] = obj;
+    // wrap back to the beginning if reaching to the end
+    end = end == arr.length - 1 ? 0 : end+1;
+  }
+  
+  public Integer poll() {
+    if (size == 0) {
+      throw new ArrayIndexOutOfBoundsException("The queue is empty");
+    }
+    size--;
+    int tmp = start;
+    //wrap back to the beginning if reaching to the end
+    start = start == arr.length - 1 ? 0 : start+1;
+    return arr[tmp];
+  }
+  
+}
