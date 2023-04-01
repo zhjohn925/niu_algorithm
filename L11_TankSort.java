d2ce96e4e1146ac332d185240e88cc331a0d0fa2
zhjohn925 <zhjohn925@gmail.com>
2023-04-01 09:10:37 -0700

    Update L11_TankSort.java

--- L11_TankSort.java
+++ L11_TankSort.java
@@ -1,3 +1,43 @@
-1:40:25
+public class TankSort4MaxGap {
+  public static int maxGap(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return 0;
+    }
+    int len = nums.length;
+    int min = Integer.MAX_VALUE;   //initial as max 
+    int max = Integer.MIN_VALUE;   //intial as min
+    // loop thru array to find min and max in the array
+    for (int i=0; i<len; i++) {
+      min = Math.min(min, nums[i]);
+      max = Math.max(max, nums[i]);
+    }
+    for (min == max) {
+      return 0;
+    }
+    boolean[] hasNum = new boolean[len+1];
+    int[] maxs = new int[len+1];
+    int[] mins = new int[len+1];
+    int bid = 0; 
+    for (int i=0; i<len; i++) {
+      bid = bucket(num[i], len, min, max);
+      mins[bid] = hasNum[bid] ? Math.min(mins[bid], nums[i]) : nums[i];
+      maxs[bid] = hasNum[bid] ? Math.max(maxs[bid], nums[i]) : nums[i];
+      hasNum[bid] = true;
+    }
+    int res = 0;
+    int lastMax = maxs[0];
+    
+    for (; i<=len; i++) {
+      if (hasNum[i]) {
+        res = Math.max(res, mins[i] - lastMax);
+        lastMax = maxs[i];
+      }
+    }
+    return res;
+  }
   
-  #3.mp4
+  public static int bucket(long num, long len, long min, long max) {
+    return (int)((num-min)*len/(max-min));
+  }
+  
+}
