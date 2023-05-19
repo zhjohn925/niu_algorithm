18a86bd34690be455793fd48008e1085bd9258b2
zhjohn925 <zhjohn925@gmail.com>
2023-05-19 15:47:38 -0700

    Update L10_HeapSort.py

--- L10_HeapSort.py
+++ L10_HeapSort.py
@@ -1,29 +1,45 @@
 def heapify(arr, n, i):
+    """
+    Helper function to heapify a subtree rooted at index i
+    in the array arr with size n.
+    """
     largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+    left = 2 * i + 1  # index of the left child
+    right = 2 * i + 2  # index of the right child
 
+    # Check if the left child is larger than the root
     if left < n and arr[left] > arr[largest]:
         largest = left
 
+    # Check if the right child is larger than the current largest
     if right < n and arr[right] > arr[largest]:
         largest = right
 
+    # If the largest element is not the root, swap them
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
+
+        # Recursively heapify the affected subtree
         heapify(arr, n, largest)
 
 
 def heap_sort(arr):
+    """
+    Heap Sort implementation.
+    """
     n = len(arr)
 
-    # Build a max heap
+    # Build a max heap by repeatedly heapifying each subtree
+    # Starting from the last non-leaf node up to the root
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
 
     # Extract elements from the heap one by one
     for i in range(n - 1, 0, -1):
+        # Swap the root (maximum element) with the last element
         arr[0], arr[i] = arr[i], arr[0]
+
+        # Heapify the reduced heap (excluding the sorted elements)
         heapify(arr, i, 0)
 
 
