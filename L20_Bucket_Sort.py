b7b60fe40930e3bac1aca0115d52c8bf97bb7d54
zhjohn925 <zhjohn925@gmail.com>
2023-05-19 19:38:48 -0700

    Update L20_Bucket_Sort.py

--- L20_Bucket_Sort.py
+++ L20_Bucket_Sort.py
@@ -26,15 +26,26 @@ def bucket_sort(arr):
     return sorted_arr
 
 
+# Insertion Sort works by dividing the input array into two parts: the sorted portion and the unsorted portion.
+# 1. Start with the first element (assume it is already sorted).
+# 2. Take the next element from the unsorted portion and insert it into the correct position in the sorted portion.
+# 3. Compare the selected element with the elements in the sorted portion, shifting them to the right until 
+#    the correct position is found.
+# 4. Repeat steps 2 and 3 until all elements in the unsorted portion are processed.
+# The insertion sort algorithm has a time complexity of O(n^2) in the worst case, 
+# where n is the number of elements in the array. However, it performs well on small lists or 
+# partially sorted lists, where it can have a time complexity close to O(n) and exhibit good 
+# performance.
 def insertion_sort(arr):
     # Perform insertion sort on the given array
     for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
+                            # sorted array is before i; unsorted array is i and after i   
+        card = arr[i]       # pick i   
+        j = i - 1           
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+            arr[j + 1] = arr[j]   # push larger than key [i] to the right
             j -= 1
-        arr[j + 1] = key
+        arr[j + 1] = card   # insert the card 
 
 
 # Example usage
