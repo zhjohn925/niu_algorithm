2ddfa48da09cd2c5da545b27946773e82a492eca
zhjohn925 <zhjohn925@gmail.com>
2023-06-08 23:00:34 -0700

    Update DynamicProg_KnapsackProblem.py

--- DynamicProg_KnapsackProblem.py
+++ DynamicProg_KnapsackProblem.py
@@ -8,6 +8,46 @@
 # Find:
 #    The most valuable combination of items that can be included in the knapsack, without exceeding its weight capacity.
 
+
+# -------------------------------------------------------------------------------------------------------
+# Method 1 ----------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------
+
+def knapsack_dynamic_programming(weights, values, capacity):
+    ''' The function starts by initializing a 2D table dp with dimensions (n+1) x (capacity+1), 
+        where n is the number of items. Each element in the table dp[i][w] represents the maximum value 
+        that can be achieved using the first i items and a knapsack capacity of w.'''
+    
+    n = len(weights)  # Number of items    
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]  # Create a 2D table to store maximum values
+
+    for i in range(1, n + 1):  # Iterate over items
+        for w in range(1, capacity + 1):  # Iterate over capacities
+            # w is current capacity                              
+            if weights[i - 1] <= w:  # If the current item can be included within the current capacity
+                # We need maximum value, so
+                # Compare the value of including the current item vs excluding the current item
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                # If the current item cannot be included within the current capacity, exclude it
+                dp[i][w] = dp[i - 1][w]
+    print(dp)            
+
+    return dp[n][capacity]  # Return the maximum value for the given capacity
+
+# Example usage
+weights = [2, 3, 4, 5]  # List of item weights
+values = [3, 4, 5, 6]  # List of item values
+capacity = 8  # Maximum capacity of the knapsack
+
+max_value = knapsack_dynamic_programming(weights, values, capacity)  # Solve the Knapsack Problem
+print("Maximum value:", max_value)  # Print the maximum value
+
+
+# -------------------------------------------------------------------------------------------------------
+# Method 2 ----------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------
+
 def knapsack_recursive(weights, values, capacity, n):
     # Base case: if either capacity or the number of items becomes 0, return 0
     if capacity == 0 or n == 0:
